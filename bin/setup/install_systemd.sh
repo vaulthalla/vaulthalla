2847cf00b6e1155bb1b158a1a63655ea7367a278
Cooper Larson <cooper.larson1@gmail.com>
2026-03-28 11:53:44 -0700

    fix dev env install

--- bin/setup/install_systemd.sh
+++ bin/setup/install_systemd.sh
@@ -1,13 +1,14 @@
 source "$(dirname "$0")/../lib/dev_mode.sh"
 
-vh_require_dev_mode
-
 echo "🛠️  Installing systemd service..."
 
 # uncomment the EnvironmentFile line in the service file
 sudo install -d -m 0755 /etc/systemd/system/vaulthalla.service.d
-printf '[Service]\nEnvironmentFile=/etc/vaulthalla/vaulthalla.env\n' \
-| sudo tee /etc/systemd/system/vaulthalla.service.d/override.conf >/dev/null
+
+if [ -f /etc/vaulthalla/vaulthalla.env ]; then
+  printf '[Service]\nEnvironmentFile=/etc/vaulthalla/vaulthalla.env\n' \
+  | sudo tee /etc/systemd/system/vaulthalla.service.d/override.conf >/dev/null
+fi
 
 sudo systemctl daemon-reload
 sudo systemctl enable --now vaulthalla.service
@@ -17,6 +18,4 @@ sudo systemctl enable --now vaulthalla-cli.service
 echo ""
 echo "🏁 Vaulthalla installed successfully!"
 
-if [[ "$DEV_MODE" == true ]]; then
-    sudo journalctl -f -u vaulthalla
-fi
+sudo journalctl -f -u vaulthalla
